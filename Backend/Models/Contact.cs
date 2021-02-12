9b91ee6adc0e62ccfe5201f28195ea9d16b6815a
eduardogarcot <eduardogarcot@gmail.com>
2021-02-12 00:53:14 -0500

    adding models

---
+++ Backend/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        
+        public long PhoneNumber{ get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Description { get; set; }
+        
+        public ICollection<Reservation> ReservationsList { get; set; }
+    }
+}
