32e3f9db79f6be27462743d9fe6bfb68bab03515
eduardogarcot <eduardogarcot@gmail.com>
2021-02-12 01:22:34 -0500

    adding Contacts controller

--- Backend/Persistence/BackendDbContext.cs
+++ Backend/Persistence/BackendDbContext.cs
@@ -7,29 +7,24 @@ using System.Threading.Tasks;
 
 namespace Backend.Persistence
 {
-    
-    namespace Backend.Persistence
+    public class BackendDbContext : DbContext
     {
-        public class BackendDbContext : DbContext
+        public BackendDbContext(DbContextOptions<BackendDbContext> options)
+            : base(options)
         {
-            public BackendDbContext(DbContextOptions<BackendDbContext> options)
-                : base(options)
-            {
-            }
-
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                modelBuilder.Entity<Reservation>()
-                    .HasOne(r => r.Contact)
-                    .WithMany(c => c.ReservationsList)
-                    .HasForeignKey(r => r.ContactId)
-                    .IsRequired()
-                    .OnDelete(DeleteBehavior.Cascade);
-            }
-
-            public DbSet<Reservation> Reservations { get; set; }
-            public DbSet<Contact> Contacts { get; set; }
         }
-    }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Contact)
+                .WithMany(c => c.ReservationsList)
+                .HasForeignKey(r => r.ContactId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
 }
