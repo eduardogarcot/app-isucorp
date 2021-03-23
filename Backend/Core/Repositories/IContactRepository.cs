c17c6076e0d8b565284fbc99a7b24d1aa5a52312
eduardogarcot <eduardogarcot@gmail.com>
2021-03-23 02:04:10 -0400

    making IContact and IReservation Repositories

---
+++ Backend/Core/Repositories/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Repositories
+{
+    interface IContactRepository: IRepository<Contact> 
+    {
+        Task<IEnumerable<Contact>> GetAllWithReservations();
+    }
+}
---
+++ Backend/Core/Repositories/IReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Repositories
+{
+    interface IReservationRepository
+    {
+        Task<Reservation> GetWithContactByIdAsync(int id);
+    }
+}
