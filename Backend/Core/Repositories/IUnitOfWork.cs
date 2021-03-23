faf622ec7d1f767bfbd055e4cdf457bb60f13024
eduardogarcot <eduardogarcot@gmail.com>
2021-03-23 02:06:22 -0400

    making IUnitOfWork

---
+++ Backend/Core/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Repositories
+{
+    interface IUnitOfWork
+    {
+        IContactRepository Contacts { get; }
+        IReservationRepository Reservations { get; }
+        Task<int> CommitAsync();
+    }
+}
