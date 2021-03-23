c532859cb97b2ee27ed8c7a3f3c0700717183d52
eduardogarcot <eduardogarcot@gmail.com>
2021-03-23 03:17:52 -0400

    creating Reservation Repository

---
+++ Backend/Persistence/Repositories/ReservationRepository.cs
@@ -0,0 +1,29 @@
+﻿using Backend.Core.Models;
+using Backend.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Persistence.Repositories
+{
+    public class ReservationRepository : Repository<Reservation> , IReservationRepository
+    {
+        public ReservationRepository(BackendDbContext context)
+            : base(context)
+        { }
+
+        private BackendDbContext BackendDbContext
+        { 
+            get { return Context as BackendDbContext; } 
+        }
+
+        public async Task<Reservation> GetWithContactByIdAsync(int id)
+        {
+            return await BackendDbContext.Reservations
+                .Include(r => r.Contact)
+                .SingleOrDefaultAsync(r => r.ContactId == id);
+        }
+    }
+}
